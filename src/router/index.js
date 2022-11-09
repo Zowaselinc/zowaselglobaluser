0fd4ecc6601bb54778f9cd2f7c3a89cec9eced1c
KingJrn <emmajnr503@protonmail.com>
2022-11-09 02:54:27 +0100

    added UserType screens

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,7 @@ import ForgotPassword from "@/pages/auth/ForgotPassword.vue";
 import CheckEmail from "@/pages/auth/CheckEmail.vue";
 import ResetPassword from "@/pages/auth/ResetPassword.vue";
 import ResetSuccess from "@/pages/auth/ResetSuccess.vue";
+import UserType from "@/pages/auth/UserType.vue";
 import Navigation from "@/layouts/partials/Navigation.vue";
 
 
@@ -106,7 +107,12 @@ const routes = [
         path: "/navigation",
         name:"Navigation",
         component:Navigation,
-    }
+    },
+    {
+        path: "/usertype",
+        name: "UserType",
+        component:UserType,
+    },
 
 ];
 
