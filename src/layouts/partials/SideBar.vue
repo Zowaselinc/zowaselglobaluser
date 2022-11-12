fb1675e5ba8cefca3330ddc516323f607b10cce0
KingJrn <emmajnr503@protonmail.com>
2022-11-12 12:34:05 +0100

    created media query module for responsive design _breakpoint.scss file

--- src/layouts/partials/SideBar.vue
+++ src/layouts/partials/SideBar.vue
@@ -1,55 +1,133 @@
 <template>
-            <!-- Side Bar -->
-            <nav id="sidebarMenu" class="collapse d-lg-block">
-            <div id="side-menu" class="list-group list-group-flush mx-3 mt-4">
-                <a href="#" class="py-2 sidebar-active" aria-current="true">
-                    <img class="img-fluid" src="@/assets/images/vectors/house.svg" alt="house"><span>Home</span>
-                </a>
-                <a href="#" class=" py-3 ripple">
-                    <img class="img-fluid" src="@/assets/images/vectors/Wallet.svg" alt="wallet"><span>Wallet</span>
-                </a>
-                <a href="#" class="py-3 ripple">
-                    <img class="img-fluid" src="@/assets/images/vectors/ChatText.svg"
-                        alt="ChatText"><span>Message</span>
+    <!-- Side Bar -->
+    <nav id="sidebarMenu" class="collapse d-md-block">
+        <div id="side-menu" class="list-group list-group-flush">
+            <a href="#" class="nav-item" aria-current="true">
+                <div class="ripple">
+                    <img class="img-fluid" src="@/assets/images/vectors/house.svg" alt="house">
+                <span>Home</span>
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
+                    <img class="img-fluid" src="@/assets/images/vectors/Wallet.svg" alt="wallet">
+                    <span>Wallet</span>
+                    <img class="arrow-symbol" src="@/assets/images/vectors/arrowSymbol.svg" alt="Arrow-Symbol">
+                    <img class="arrow-symbol carretUp" src="@/assets/images/vectors/arrowupsymbol.svg"
+                        alt="Arrow-Symbol">
+                </div>
+                <!-- drop menu -->
+                <div class="drop-menu wallet-menu">
+                    <div>Credit wallet</div>
+                    <div>Withdrawal</div>
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
+                    <img class="img-fluid" src="@/assets/images/vectors/ChatText.svg" alt="ChatText"><span>Message</span>
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
+                    <img class="img-fluid" src="@/assets/images/vectors/Storefront.svg"
+                    alt="marketplace"><span>Marketplace</span>
+                <img class="arrow-symbol" src="@/assets/images/vectors/arrowSymbol.svg" alt="Arrow-Symbol">
+                <img class="arrow-symbol carretUp" src="@/assets/images/vectors/arrowupsymbol.svg" alt="Arrow-Symbol">
+                </div>
+                <!-- drop menu -->
+                <div class="drop-menu marketplace-menu">
+                    <div>Input market</div>
+                    <div>Crop market</div>
+                    <div>Logistics</div>
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
+                    <img class="img-fluid" src="@/assets/images/vectors/marketPlace.svg" alt="Credit Scoring"><span>Credit
+                    Scoring</span>
+                <img class="arrow-symbol" src="@/assets/images/vectors/arrowSymbol.svg" alt="Arrow-Symbol">
+                <img class="arrow-symbol carretUp" src="@/assets/images/vectors/arrowupsymbol.svg" alt="Arrow-Symbol">
+                </div>
+                <!-- drop menu -->
+                <div class="drop-menu scoring-menu">
+                    <div>Credit score point system</div>
+                    <div>credit scor analysis</div>
+                    <div>Loans</div>
+                    <div>Score Analysis</div>
+                </div>
 
-                </a>
-                <a href="#" class="py-3 ripple">
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
                     <img class="img-fluid" src="@/assets/images/vectors/File.svg" alt="file"><span>Invoice</span>
 
-                </a>
-                <a href="#" class=" py-3 ripple">
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
                     <img class="img-fluid" src="@/assets/images/vectors/Folder.svg" alt="folder"><span>API and
-                        subscription</span>
+                    subscription</span>
+                </div>
+                <!-- drop menu -->
+                <div class="drop-menu api-menu">
+                    <div>service subscription</div>
+                    <div>Renewal manager</div>
+                    <div>Traffic & usage stats</div>
+                    <div>Changelog notification</div>
 
-                </a>
-                <a href="#" class=" py-3 ripple">
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+               <div class="ripple">
+                <img class="img-fluid" src="@/assets/images/vectors/Data.svg" alt="Data"><span>Data</span>
+                <img class="arrow-symbol" src="@/assets/images/vectors/arrowSymbol.svg" alt="Arrow-Symbol">
+                <img class="arrow-symbol carretUp" src="@/assets/images/vectors/arrowupsymbol.svg" alt="Arrow-Symbol">
+               </div>
+
+                <!-- drop menu -->
+                <div class="drop-menu data-menu">
+                    <div>Farmer Data</div>
+                    <div>Market report</div>
+                    <div>Crop data stats</div>
+                    <div>Seed data</div>
+
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
                     <img class="img-fluid" src="@/assets/images/vectors/Info.svg" alt="info"><span>Support</span>
 
-                </a>
-                <a href="#" class=" py-3 ripple">
-                    <img class="img-fluid" src="@/assets/images/vectors/GearSixBold.svg"
-                        alt="settings"><span>Settings</span>
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+               <div class="ripple">
+                <img class="img-fluid" src="@/assets/images/vectors/GearSixBold.svg"
+                    alt="settings"><span>Settings</span>
+               </div>
 
-                </a>
-                <a href="#" class=" py-3 ripple">
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
                     <img class="img-fluid" src="@/assets/images/vectors/User.svg" alt="user"><span>My account</span>
 
-                </a>
-                <a href="#" class=" py-3 ripple">
-                    <img class="img-fluid" src="@/assets/images/vectors/SignOut.svg" alt="SignOut"><span>Logout</span>
+                </div>
+            </a>
+            <a href="#" class="nav-item">
+                <div class="ripple">
+                <img class="img-fluid" src="@/assets/images/vectors/SignOut.svg" alt="SignOut"><span>Logout</span>
 
-                </a>
-            </div>
-        </nav>
+                </div>
+            </a>
+        </div>
+    </nav>
 </template>
 
 <script>
-    export default {
-        name: "SideBar"
-    }
+export default {
+    name: "SideBar"
+}
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/navigation";
-
 </style>
