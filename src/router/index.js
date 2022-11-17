dde426197150734d6e0c20a5c1546a5e075feea3
Ctrixx <ctrixx01@gmail.com>
2022-11-17 08:32:29 +0100

    Modified routes, changed redirect after welcome

--- src/router/index.js
+++ src/router/index.js
@@ -90,6 +90,11 @@ const routes = [
         component:UserType,
     },
     {
+        path: "/dashboard/merchant",
+        name: "MerchantDashboard",
+        component:CorporateDashboard,
+    },
+    {
         path: "/dashboard/corporate",
         name: "CorporateDashboard",
         component:CorporateDashboard,
