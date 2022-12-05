9082d75b8241b0932f25b9cb756e2c63fcc63b54
TeamZowasel <113076845+TeamZowasel@users.noreply.github.com>
2022-12-05 15:19:41 +0100

    added eslint ignore

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
    publicPath: process.env.NODE_ENV === 'production'
   ? '/docs/1.0/'  // This is whatever your path from the root is
-  : '/'
+  : '/',
+   chainWebpack: config => {
+        config.module.rules.delete('eslint');
+    }
   
 })
