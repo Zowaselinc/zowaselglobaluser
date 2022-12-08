a79589e465aad07429b0bcb08b0fdd0e066bb02d
Ctrixx <ctrixx01@gmail.com>
2022-12-08 15:51:14 +0100

    Fixed abstract name error

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 export default {
-    //BASE_URL : "http://localhost:3000/api/"
-    BASE_URL : "https://zowaseluser.loclx.io/api/"
+    BASE_URL : "http://localhost:3000/api/"
+    // BASE_URL : "https://zowaseluser.loclx.io/api/",
+    // BASE_URL : "https://a5731c4b65e1.eu.ngrok.io/api/"
 }
