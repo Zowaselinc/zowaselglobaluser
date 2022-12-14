bda9f858b5b0fecba9eded56627e6a1f69266fce
Ctrixx <ctrixx01@gmail.com>
2022-12-14 13:31:57 +0100

    Changed vue.config and base url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-    BASE_URL : "https://cfc80ea284ba.eu.ngrok.io/api/"
+    BASE_URL : "https://isscunseci.us-east-1.awsapprunner.com/api/"
     // BASE_URL : "https://zowaseluser.loclx.io/api/",
     // BASE_URL : "https://a5731c4b65e1.eu.ngrok.io/api/"
 }
