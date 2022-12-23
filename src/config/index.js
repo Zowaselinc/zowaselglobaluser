58aaea9502bac64927d2b9e704aa2a59b90fe82c
Ctrixx <ctrixx01@gmail.com>
2022-12-23 16:28:57 +0100

    Changed base_url to production

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
-    BASE_URL : "https://309b9a4c05ee.eu.ngrok.io/api/",
-    // BASE_URL : "https://isscunseci.us-east-1.awsapprunner.com/api/"
+    // BASE_URL : "https://309b9a4c05ee.eu.ngrok.io/api/",
+    BASE_URL : "https://isscunseci.us-east-1.awsapprunner.com/api/",
     // BASE_URL : "https://zowaseluser.loclx.io/api/",
     // BASE_URL : "https://a5731c4b65e1.eu.ngrok.io/api/"
     MAIN_URL : "http://localhost:8080/"
