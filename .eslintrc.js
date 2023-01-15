eb23b368e9bbd2b2c484f1dab2bad43524c5d89f
Ctrixx <ctrixx01@gmail.com>
2023-01-15 23:10:24 +0100

    Added eslint-prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
-module.exports ={
-    extends : [
-        'plugin:vue/vue3-recommended'
-    ],
-    rules:{
-        "vue/multi-word-component-names" : 0,
-        "vue/multiline-html-element-content-newline" : 0,
-        "vue/html-indent": 0
-    }
-}
+module.exports = {
+  extends: ["plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
+  rules: {
+    "vue/multi-word-component-names": 0,
+    "vue/multiline-html-element-content-newline": 0,
+    "vue/html-indent": 0,
+  },
+};
