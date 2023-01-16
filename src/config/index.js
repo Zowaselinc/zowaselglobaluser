9ccb15e387cc4c6c62d16aeae3263c7c7e7b1f22
Ctrixx <ctrixx01@gmail.com>
2023-01-16 11:32:17 +0100

    Linted config files

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
-    //BASE_URL : "https://37cff82c1a5e.eu.ngrok.io/api/",
-    BASE_URL : "https://isscunseci.us-east-1.awsapprunner.com/api/",
-    // BASE_URL : "https://zowaseluser.loclx.io/api/",
-    // BASE_URL : "https://a5731c4b65e1.eu.ngrok.io/api/"
-    MAIN_URL : "http://localhost:8080/"
-}
+  //BASE_URL : "https://37cff82c1a5e.eu.ngrok.io/api/",
+  BASE_URL: "https://isscunseci.us-east-1.awsapprunner.com/api/",
+  // BASE_URL : "https://zowaseluser.loclx.io/api/",
+  // BASE_URL : "https://a5731c4b65e1.eu.ngrok.io/api/"
+  MAIN_URL: "http://localhost:8080/",
+};
