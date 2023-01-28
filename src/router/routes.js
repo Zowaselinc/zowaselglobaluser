540d11dca648ac4edd80f631513bfbba62039892
KingJrn <emmajnr503@protonmail.com>
2023-01-28 23:30:02 +0100

    added preloader

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,13 @@
 const Routes = {
-  HOME: "/dashboard",
-  ACCOUNT: "/dashboard/account",
-  NOTIFICATIONS: "/dashboard/notifications",
-  MESSAGES: "/dashboard/messages",
-  NEGOTIATIONS: "/dashboard/negotiations",
-  SUBSCRIPTION: "/dashboard/subscription",
-  CREDIT_WALLET: "/dashboard/creditwallet",
-  SETTINGS: "/dashboard/settings",
-  MARKETPLACE: "/dashboard/marketplace",
+    HOME: "/dashboard",
+    ACCOUNT: "/dashboard/account",
+    NOTIFICATIONS: "/dashboard/notifications",
+    MESSAGES: "/dashboard/messages",
+    NEGOTIATIONS: "/dashboard/negotiations",
+    SUBSCRIPTION: "/dashboard/subscription",
+    CREDIT_WALLET: "/dashboard/creditwallet",
+    SETTINGS: "/dashboard/settings",
+    MARKETPLACE: "/dashboard/marketplace",
 };
 
 export default Routes;
